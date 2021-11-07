ea7dc394599152d89d41e4f393c892b49502d568
Michael Kazan <michaelrkazan@gmail.com>
2021-11-07 15:03:21 -0500

    Internet Ninja

---
+++ src/ninja1/Day1ninja.java
@@ -0,0 +1,13 @@
+package ninja1;
+
+public class Day1ninja {
+
+	public static void main(String[] args) {
+	
+		System.out.println("helpless\n helpfull");
+			
+		}
+
+	}
+
+
---
+++ src/ninja1/Day2Ninja.java
@@ -0,0 +1,10 @@
+package ninja1;
+
+public class Day2Ninja {
+	public static void main(String [] args) {
+		
+		System.out.println("Kale oglo"); 
+	}
+	
+
+}
